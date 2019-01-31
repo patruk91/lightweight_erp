58acb1b101814063b46666ed53a4e1f1a1f80918
Konrad Krzysztofiak <krzysztofiakk@gmail.com>
2019-01-31 15:57:29 +0100

    Update record in table by given option by user

--- store/store.py
+++ store/store.py
@@ -115,17 +115,38 @@ def remove(table, id_):
 def update(table, id_):
     """
     Updates specified record in the table. Ask users for new data.
-
-    Args:
-        table: list in which record should be updated
-        id_ (str): id of a record to update
-
-    Returns:
-        list: table with updated record
+    :param table: text file where are included some information.
+    :param id_: id of a record to update
+    :return: list of list with updated record
     """
+    searched_record = [record for record in table if id_ in record]
+    ui.print_table(searched_record, title_list)
+    searched_record = searched_record[0]  # unpack from list of lists
+    id_place = 1
+    # due to id in on the 0 position in list
 
-    # your code
+    i = 0
+    while i < 1:
+        user_input = input("What do you want change?").lower()
+        if user_input in update_options:
+            chosen_option = update_options.index(user_input) + id_place
+            new_data = input("Actual " + user_input + ": "
+                             + searched_record[chosen_option]
+                             + "\nEnter new: ")
 
+            if chosen_option > 2:
+                searched_record[chosen_option] = new_data
+                i += 1
+            elif common.check_if_input_is_number(new_data) and common.check_if_data_is_in_range(
+                    chosen_option - + id_place, new_data, border_conditions):
+                searched_record[chosen_option] = new_data
+                i += 1
+            else:
+                print("some kind of error, to wide range for day month year etc")
+        else:
+            print("Provide correct value")
+    data_manager.write_table_to_file(file_name, table=table)
+    ui.print_table([searched_record], title_list)
     return table
 
 
